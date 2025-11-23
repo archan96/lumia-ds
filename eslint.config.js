a4cfeb9f3b93bad2ca34ee9d66f751a95a04cf1c
archan96 <archan.ray2011@gmail.com>
2025-11-23 10:06:30 +0530

    feat: Initialize monorepo structure with core package, TypeScript, ESLint, Prettier, and build setup.

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+const js = require('@eslint/js');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+const globals = require('globals');
+
+module.exports = [
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js', 'eslint.config.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
---
+++ packages/core/src/index.ts
@@ -0,0 +1,2 @@
+export const hello = 'world';
+export const add = (a: number, b: number) => a + b;
